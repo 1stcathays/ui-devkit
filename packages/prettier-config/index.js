16849c1f28bafaaa1ce89e5ad311ce293b1db5fe
Ahmed <ahmedhussein_96@hotmail.com>
2024-08-08 12:29:26 +0100

    feat: set up publishing of packages

---
+++ packages/prettier-config/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSameLine: false,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+};
